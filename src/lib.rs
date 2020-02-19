02d23a3aa10502dbfeb752b723abc046febb94b5
Paul Sajna <sajattack@gmail.com>
2020-02-19 13:10:16 -0800

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 struct Boomer {}
 
 fn ok_boomer() -> Result<Boomer, ()> {
-    Ok(Boomer{})
+    Ok(Boomer {})
 }
 
 fn all_star(i_aint_the_sharpest_tool_in_the_shed: Option<i32>) {
@@ -13,20 +13,21 @@ fn all_star(i_aint_the_sharpest_tool_in_the_shed: Option<i32>) {
     let they_dont_stop_coming = true;
     let gold = true;
     let all_that_glitters = true;
-        if let Some(body_once_told_me_the_world_was_gonna_roll_me) = 
-            i_aint_the_sharpest_tool_in_the_shed {
-                'the_mold: for she_was_looking_kinda_dumb_with_her_finger_and_her_thumb in 
-                    the_shape_of_an_l_on_her_forehead {
-                    if the_years_start_coming && they_dont_stop_coming ||
-                        fed_to_the_rules && hit_the_ground_running
-                    {
-                        let didnt_make_sense = !live_for_fun;
-                        if (all_that_glitters == gold) {
-                            break 'the_mold;
-                        }
-                        
-                    }
+
+    if let Some(body_once_told_me_the_world_was_gonna_roll_me) =
+        i_aint_the_sharpest_tool_in_the_shed
+    {
+        'the_mold: for she_was_looking_kinda_dumb_with_her_finger_and_her_thumb in
+            the_shape_of_an_l_on_her_forehead
+        {
+            if the_years_start_coming && they_dont_stop_coming
+                || fed_to_the_rules && hit_the_ground_running
+            {
+                let didnt_make_sense = !live_for_fun;
+                if (all_that_glitters == gold) {
+                    break 'the_mold;
                 }
             }
+        }
+    }
 }
-
